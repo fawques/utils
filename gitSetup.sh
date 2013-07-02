b2b8471d7db6f1383be987f230765ee54162735e
Víctor Guzmán <fawques@gmail.com>
2013-07-02 18:06:33 +0200

    git setup

---
+++ gitSetup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+git config --global user.name "Víctor Guzmán"
+git config --global user.email "fawques@gmail.com"
+git config --global color.ui true
+git config --global help.autocorrect 1
+git config --global alias.ci commit
+git config --global alias.co checkout
+git config --global alias.st status
+git config --global alias.br branch
