e3d7aa0597aad8934e2845a521455ca6e6759385
Víctor Guzmán <fawques@gmail.com>
2014-09-28 19:31:23 +0100

    añadido ejemplo al script de cambio de fecha de commits

--- commitDateChanger.sh
+++ commitDateChanger.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # Script para cambiar la fecha de un commit a la fecha que se le pase por parámetros, fecha actual si no se le pasa ninguna
+# Ejemplo: $ ./commitDateChanger "1970-01-01 12:45 +0100"
 export LANG=en_EN.UTF-8
 date=`date -d "$@" "+%c %z"`
 echo $date
